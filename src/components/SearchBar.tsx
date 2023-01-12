f99bf853fa5d3fe963afd1aadb417910c528e393
Arda Örkin <ardaorkin@gmail.com>
2023-01-12 16:37:24 +0300

    placeholder

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -4,6 +4,7 @@ import { ISearchBarProps } from "../types";
 export default function SearchBar({ onSearch }: ISearchBarProps) {
   return (
     <Input.Search
+      placeholder="Search for notes..."
       onChange={(event) => onSearch(event.target.value)}
       style={{ width: 200, marginLeft: 5 }}
     />
