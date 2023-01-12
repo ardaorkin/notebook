192a4d83a405f1bced0c56106f3b75819ba665a3
Arda Örkin <ardaorkin@gmail.com>
2023-01-12 20:34:05 +0300

    use constant action types

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,15 +1,16 @@
 import { INote, INoteAction } from "../types";
+import { ADD_NOTE, DELETE_NOTE, UPDATE_NOTE } from "./actionTypes";
 
 export const reducer = (state: INote[], action: INoteAction): INote[] => {
   switch (action.type) {
-    case "ADD_NOTE":
+    case ADD_NOTE:
       return [action.payload, ...state];
-    case "UPDATE_NOTE":
+    case UPDATE_NOTE:
       const copy = [...state];
       const { index, ...rest } = action.payload;
       copy[index] = rest;
       return copy;
-    case "DELETE_NOTE":
+    case DELETE_NOTE:
       return [
         ...state.slice(0, action.payload),
         ...state.slice(action.payload + 1),
